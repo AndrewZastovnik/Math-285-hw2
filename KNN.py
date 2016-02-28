51643f02c0ab721aac71ec5c907fad052527dbc0
Andrew <Haldfhaklfa@gmail.com>
2016-02-28 10:50:52 -0800

    These are my latest files

--- KNN.py
+++ KNN.py
@@ -4,7 +4,7 @@ from TicToc import tic
 from TicToc import toc
 
 
-def KNN(I, L, x, k):
+def KNN(I, L, x, k,weights = 1):
     from scipy import stats
     from scipy.spatial.distance import cdist
     """
@@ -18,14 +18,44 @@ def KNN(I, L, x, k):
     sizex = len(np.atleast_2d(x))
     label = np.zeros((k,sizex))
     for rowsx in range(0, sizex):
-        dists = cdist(I, np.atleast_2d(x[rowsx]), metric='cityblock')
+        tic()
+        dists = cdist(I, np.atleast_2d(x[rowsx]), metric='euclidean')
         # Now we should have all our distances in our dist array
         # The next step is to use this info to classify each unknown obj
-        k_smallest = np.argpartition(dists,tuple(range(1,k)),axis=None)
-        for i in range(0,k):
-            print(L[k_smallest[:i+1]])
-            label[i,rowsx] = stats.mode(L[k_smallest[:i+1]])[0]
+        k_smallest = np.argpartition(dists,tuple(range(1,k+1)),axis=None)
+        if weights == 1:
+            for i in range(0,k):
+                label[i,rowsx] = stats.mode(L[k_smallest[:i+1]])[0]
+        else:
+            labs = np.unique(L)
+            myimage = x[rowsx].reshape(28,28)
+            for i in range(k):
+                lab_weighted = np.zeros(np.unique(L).shape[0])
+                d = dists[k_smallest[:i+2]][:,0]
+                weight_function = np.add(np.divide(d, np.subtract(np.min(d),np.max(d))),1-np.min(d)/np.subtract(np.min(d),np.max(d)))
+                for p in range(0,labs.shape[0]):
+                    indices = inboth(np.arange(0,L.shape[0])[L == labs[p]],k_smallest[:i+2])
+                    lab_weighted[p]= np.sum(np.multiply(weight_function,indices))
+                label[i,rowsx] = labs[np.argmax(lab_weighted)]
+        toc()
+        print(rowsx)
     return label
+
+def plot_weights(weight_function,I,myimage,k_smallest,i):
+    import pylab as plt
+    plt.subplot(2,1,1)
+    plt.plot(range(weight_function.shape[0]),weight_function)
+    plt.subplot(2,1,2)
+    myimage = np.hstack((myimage,I[k_smallest[i]].reshape(28,28)))
+    plt.imshow(myimage,cmap = 'gray',)
+    plt.show()
+
+def inboth(list1,list2):
+    index = np.zeros(list2.shape)
+    for i in range(list2.shape[0]):
+        if list2[i] in list1:
+            index[i] = 1
+    return index
 
 def mfoldX(I, L, m, maxk):
     # I is the trainset
@@ -92,6 +122,12 @@ def local_kmeans_class(I, L, x, k):
             label[rowsx,i] = label_order[k_smallest[0]]
     return label
 
+def class_error_rate(pred_labels,true_labels):
+    error = np.zeros(pred_labels.shape[0])
+    for i in range(pred_labels.shape[0]):
+        error[i] = sum(pred_labels[i] != true_labels)/pred_labels.shape[1]
+    return error
+
 train_Images = pickle.load(open('mnistTrainI.p', 'rb'))
 train_Labels = pickle.load(open('mnistTrainL.p', 'rb'))
 test_Images = pickle.load(open('mnistTestI.p', 'rb'))
@@ -99,11 +135,16 @@ test_labels = pickle.load(open('mnistTestL.p', 'rb'))
 """
 m = local_kmeans_class(train_Images,train_Labels,test_Images[:100],10)
 pickle.dump(m, open('localkmeans.p', 'wb'))
-
-label = KNN(train_Images, train_Labels, test_Images, 10)
-pickle.dump(label, open('KNN_city.p', 'wb'))
+label = KNN(train_Images, train_Labels, test_Images[:10], 12,0)
+print(label)
+#pickle.dump(label, open('KNN_city.p', 'wb'))
 """
-
+label = pickle.load(open('kNNWeight.p', 'rb'))
+errors = class_error_rate(label,test_labels)
+import pylab as plt
+plt.plot(range(12),errors)
+plt.show()
+"""
 label = pickle.load(open('kNNWeight.p', 'rb'))
 from sklearn.metrics import confusion_matrix
 import pandas
@@ -117,7 +158,7 @@ import matplotlib.pyplot as plt
 plt.plot(range(10),error)
 plt.show()
 print(pandas.DataFrame(x,range(10),range(10)))
-
+"""
 """
 tic()
 m = mfoldX(train_Images[:6000], train_Labels[:6000], 6, 10)
--- PCA.py
+++ PCA.py
@@ -1,7 +1,7 @@
 import numpy as np
 import pickle
 import pylab as plt
-from NearestNeighbors import mfoldX
+from NearestNeighbors import mfoldX,KNN
 
 class center_matrix_SVD:
     # A class to store our information about our centered matrix
@@ -18,29 +18,88 @@ class center_matrix_SVD:
         self.U, self.s, self.V = np.linalg.svd(self.a_centered,full_matrices = False)
         self.PCA = self.U@np.diagflat(self.s)
 
+
 def dump_the_svd():
     # Gets SVD for the training dataset
     train_Images = pickle.load(open('mnistTrainI.p', 'rb'))
     train = center_matrix_SVD(train_Images)
     pickle.dump(train,open('Training SVD Data','wb'))
 
+def class_error_rate(pred_labels,true_labels):
+    # for calculating the error rate
+    # Also returns a index vector with the position of incorrectly labeled images
+    error = np.zeros(pred_labels.shape[0])
+    error_index = np.zeros((pred_labels.shape[0],pred_labels.shape[1]))
+    for i in range(pred_labels.shape[0]):
+        error[i] = sum(pred_labels[i] != true_labels)/pred_labels.shape[1]
+        print([pred_labels[i] != true_labels])
+        plt.scatter(np.arange(60000),[pred_labels[i] != true_labels].shape)
+        plt.show()
+        error_index[i] = np.arange(pred_labels.shape[1])[pred_labels[i] != true_labels]
+    return error, error_index
+
+def MFold_plots():
+    # Mfold plots
+    err = pickle.load(open('MFolderrors50','rb'))
+    err2 = pickle.load(open('MFolderrors','rb'))
+    err3 = pickle.load(open('MFolderrors154','rb'))
+    plt.figure()
+    ax = plt.subplot(111)
+    ax.scatter(np.arange(10)+1,err,color = 'Green',s=200,label = '82.5%')
+    ax.scatter(np.arange(10)+1,err3,color = 'Yellow',s=150,label = '95%')
+    ax.scatter(np.arange(10)+1,err2,color = 'Red',s=100,label = 'Full')
+    ax.grid(1)
+    plt.title('Scatter plot of the three error rates')
+    plt.legend(loc='upper right')
+    plt.show()
+
+def do_KNN(x,train_Labels):
+    test_Images = pickle.load(open('mnistTestI.p', 'rb'))
+    test_Images_Center = np.mean(test_Images,axis=0).reshape(1,test_Images.size[1])
+    Knn_labels = KNN(x.PCA,train_Labels,test_Images_Center@x.V,10)
+    pickle.dump(Knn_labels,open('Knn_Full','wb'))
+    Knn_labels = KNN(x.PCA[:,:154],train_Labels,test_Images_Center@x.V[:,:154],10)
+    pickle.dump(Knn_labels,open('Knn_154','wb'))
+    Knn_labels = KNN(x.PCA[:,:50],train_Labels,test_Images_Center@x.V[:,:50],10)
+    pickle.dump(Knn_labels,open('Knn_50','wb'))
+
+def KNN_Plots():
+    # KNN plots
+    labels_50 = pickle.load(open('KNN_50','rb'))
+    labels_154 = pickle.load(open('KNN_154','rb'))
+    labels_Full = pickle.load(open('KNN_Full','rb'))
+    test_labels = pickle.load(open('mnistTestL.p', 'rb'))
+    error_50, error_50_index = class_error_rate(labels_50,test_labels)
+    print(error_50_index)
+
+
 def main():
-    # skip is a bool list that says what functions I want to run
-    skip = [0,1,0]
-    if skip[0]:
+    # the if 0 or 1 stuff is so that i can run only the code i want without deleting or commenting out code
+    if 0:
         dump_the_svd()
-    if skip[1]:
+    if 1: # do stuff after getting the svd iformation
         x = pickle.load(open('Training SVD Data','rb'))
         train_Labels = pickle.load(open('mnistTrainL.p', 'rb'))
-        merror = mfoldX(x.PCA,train_Labels,6,10)
-        pickle.dump(merror,open('MFoldErrors','wb'))
-    #test_Images = pickle.load(open('mnistTestI.p', 'rb'))
-    #test_labels = pickle.load(open('mnistTestL.p', 'rb'))
-    if skip[2]:
-        plt.imshow(x.centers.reshape(28,28))
+        if 0: #Do m-folds
+            merror = mfoldX(x.PCA[:,:50],train_Labels,6,10)
+            pickle.dump(merror,open('MFoldErrors154','wb'))
+        if 1: #Do K-nearest Neighbors
+            do_KNN(x,train_Labels)
+    if 0: # Plot stuff
+        KNN_Plots()
+        MFold_plots()
+        plt.imshow(x.PCA[0,:].reshape(28,28),interpolation = 'none')
         plt.show()
         #plt.imshow(train.a_centered[1,:].reshape(28,28))
         #plt.show()
+        plt.scatter(np.arange(784),x.PCA[0,:])
+        plt.show()
+        plt.scatter(np.arange(154),x.U[0,0:154])
+        plt.show()
+        plt.scatter(np.arange(154),x.s[0:154])
+        plt.show()
+        plt.imshow(x.V[0,:].reshape(28,28))
+        plt.show()
 
 
 if __name__ == "__main__":
@@ -50,12 +109,4 @@ if __name__ == "__main__":
 """
 Plotting code
         train_Images = x.U[:,0:154]@np.diagflat(x.s)[0:154,0:154]
-        plt.scatter(np.arange(154),train_Images[0,:])
-        plt.show()
-        plt.scatter(np.arange(154),x.U[0,0:154])
-        plt.show()
-        plt.scatter(np.arange(154),x.s[0:154])
-        plt.show()
-        plt.imshow(x.V[0,:].reshape(28,28))
-        plt.show()
 """
